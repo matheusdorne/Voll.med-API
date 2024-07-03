977ac5d2eaa74ee37086716876ca0826bf8a0186
Matheus Dornelles <matheus.dorne@gmail.com>
2024-07-03 09:34:53 -0300

    up

--- api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -15,6 +15,18 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("FILTRO CHAMADO");
+        var tokenJWT = recuperarToken(request);
+
+
+
+        filterChain.doFilter(request, response); // Chama o próximo filtro
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null){
+            throw new RuntimeException("Token KWT não enviado!");
+        }
+        return authorizationHeader.replace("Bearer","");
     }
 }
