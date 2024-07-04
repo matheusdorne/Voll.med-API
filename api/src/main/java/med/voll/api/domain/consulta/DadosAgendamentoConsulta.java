f2786e9389e01731e4c7613c645c079abe2590c8
Matheus Dornelles <matheus.dorne@gmail.com>
2024-07-04 10:11:57 -0300

    consultas
    iniciado criação da função de agendamento

---
+++ api/src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -0,0 +1,18 @@
+package med.voll.api.domain.consulta;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record DadosAgendamentoConsulta(
+        Long idMedico,
+
+        @NotNull
+        Long idPaciente,
+
+        @NotNull
+        @Future //Verifica se a data é no futuro
+        LocalDateTime data) {
+}
+
