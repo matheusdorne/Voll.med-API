08860d8eb9a50bd28379223f6327ffefdce84edb
Matheus Dornelles <matheus.dorne@gmail.com>
2024-07-09 16:00:25 -0300

    consulta medico
    Criado query personalizada no MedicoRepository

--- api/src/main/java/med/voll/api/controller/ConsultaController.java
+++ api/src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,9 +1,12 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.AgendaDeConsultas;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/consultas")
 public class ConsultaController {
 
+    @Autowired
+    private AgendaDeConsultas agenda;
 
+
+    @PostMapping
     public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
 
-        System.out.println(dados);
+        agenda.agendar(dados);
         return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null ,null));
     }
 }
